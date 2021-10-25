7789317efd3644f1984a28c65dbb1881705f049c
disconform <coreycline85@gmail.com>
2021-10-25 14:16:25 -0700

    creationg array foir assignment1

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,23 @@
+const breakfastFoods = ['bacon', 'eggs', 'toast', 'coffee', 'yogurt'];
+
+console.log(breakfastFoods);
+
+breakfastFoods.push('orange juice');
+
+const breakfastFoods2 = ['bacon', 'eggs', 'toast', 'coffee', 'yogurt'];
+
+breakfastFoods2.push('orange juice');
+
+console.log(breakfastFoods2);
+
+const breakfastFoods3 = ['bacon', 'eggs', 'toast', 'coffee', 'yogurt'];
+
+breakfastFoods3.pop(2);
+
+console.log(breakfastFoods3);
+
+let breakfastFoods4 = ['bacon', 'eggs', 'toast', 'coffee', 'yogurt'];
+
+const breakfastFoodsList = breakfastFoods4.join(',');
+
+console.log(breakfastFoodsList);
