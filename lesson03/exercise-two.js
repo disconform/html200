10386d329b3b1c0cf5709cd00288fde73f8ad680
disconform <coreycline85@gmail.com>
2021-11-03 13:00:07 -0700

    lesson3 exercise2 complete

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,6 @@
+let breakfastFoods = ['bacon', 'and', 'eggs', 'are', 'delicious.'];
+
+const breakfastFoodsList= breakfastFoods.join(' ');
+
+console.log (breakfastFoodsList);
+console.log (breakfastFoods[2]);
